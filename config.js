0a87e18167cd0b330e0481f477834869a7b1c90e
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-08-25 14:19:57 +0200

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: '',
+  db: 'mongodb+srv://admin:xm1gLDdSshAnxmle@cluster0.82pci.mongodb.net/<dbname>?retryWrites=true&w=majority',
   keySession: ['TWOJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000
 }
